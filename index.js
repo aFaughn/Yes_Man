a3301dc76df095b05585ffb314b0ddb6b2bfbb1d
aFaughn <allenfaughn@gmail.com>
2023-07-03 15:25:25 -0500

    set up dynamic retrieval of command files

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 require('dotenv').config()
-const {Client, GatewayIntentBits } = require(`discord.js`)
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Collection, Events, GatewayIntentBits } = require(`discord.js`)
+const token = process.env.API_KEY;
+
+// Declare permissions that our bot will need in order to perform it's functions.
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, 
@@ -8,11 +13,26 @@ const client = new Client({
     ] 
  });
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file)
+  const command = require(filePath);
+
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
+
 // Register an event so that when the bot is ready, it will log a messsage to the terminal
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 })
 
 // client.login logs the bot in and sets it up for use. You'll enter your token here.
-const token = process.env.API_KEY;
 client.login(token);
