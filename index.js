df4f9ea0f5bc201d0d78de745d5b10ff6398008e
aFaughn <allenfaughn@gmail.com>
2023-07-03 14:59:13 -0500

    updated and added ping slash command

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const Discord = require(`discord.js`)
-const client = new Discord.Client();
-import 'os'
+require('dotenv').config()
+const {Client, GatewayIntentBits } = require(`discord.js`)
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ] 
+ });
 
 // Register an event so that when the bot is ready, it will log a messsage to the terminal
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 })
 
-// Register an event to handle incoming messages
-client.on('message', async msg => {
-    if (msg.author.bot) {
-        return
-    }
-
-    // Check if the message starts with '!hello' and respond with 'world!' if it does.
-    if(msg.content.startsWith("!hello")) {
-        msg.reply("world!")
-    }
-})
-
 // client.login logs the bot in and sets it up for use. You'll enter your token here.
-client.login(process.env.API_KEY);
+const token = process.env.API_KEY;
+client.login(token);
