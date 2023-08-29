c812f52ff1eaf05c5726554481b9aa0e302604ce
aFaughn <allenfaughn@gmail.com>
2023-08-29 12:08:29 -0500

    created connection to docker DB through Sequelize

---
+++ database/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: `postgres`,
+});
+
+module.exports = sequelize;
