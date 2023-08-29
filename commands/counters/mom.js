b208a5c4bbf1a1d8312553e0aadf1b58a3e1c12f
aFaughn <allenfaughn@gmail.com>
2023-08-29 11:27:14 -0500

    disabled the counter commands

--- commands/counters/mom.js
+++ commands/counters/mom.js
@@ -8,6 +8,7 @@ module.exports = {
 		.setDescription('Increment\'s the amount of times Quenton has made a "your mom" joke.'),
 	async execute(interaction) {
         counter += 1;
-		await interaction.reply(`Quenton has made the "your mom" joke ${counter} times! It's always so funny!`);
+		// await interaction.reply(`Quenton has made the "your mom" joke ${counter} times! It's always so funny!`);
+		await interaction.reply(`Counters are temporarily disabled until I get linked to a **PostgreSQL** database. Sorry!`)
 	},
 };
