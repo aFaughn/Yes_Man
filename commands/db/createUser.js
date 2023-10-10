bbe1cfd86f77dbdab785fd08635f2878a90a4142
aFaughn <allenfaughn@gmail.com>
2023-10-09 19:26:30 -0500

    Revert "properly initialized db through sequelize-cli"
    This reverts commit a31a065dad03da8b6fbd4aa2429535afe38e3eef.

--- commands/db/createUser.js
+++ commands/db/createUser.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, Message, InteractionCollector } = require("discord.js");
 
-const { User } = require('../../database/models')
+const {User} = require('../../database/models')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,43 +12,40 @@ module.exports = {
 				.setRequired(false)),
 	async execute(interaction) {
 		// Grab additional arguements if there are any
-		// const args = await interaction.options.getString('username')
+		const args = await interaction.options.getString('username')
 
-		// if (!args) {
-		// 	// Check if user already exists
-		// 	const user = await User.findAll({
-		// 		where: {
-		// 			username: interaction.user.username
-		// 		}
-		// 	})
-		// 	if (!user[0]) {
-		// 		// Create new user
-		// 		const newUser = await User.create({
-		// 			username: interaction.user.username
-		// 		})
-		// 		await interaction.reply(`Created a new user with username: ${interaction.user.username}`)
-		// 	} if (user[0]) {
-		// 		// Alert sender the user already exists
-		// 		await interaction.reply('User already exists.')
-		// 	}
-		// } else {
-		// 	// Check if user already exists
-		// 	const user = await User.findAll({
-		// 		where: {
-		// 			username: args
-		// 		}
-		// 	})
-		// 	if (!user[0]) {
-		// 		const newUser = await User.create({
-		// 			username: args
-		// 		})
-		// 		await interaction.reply(`Created a new user with username: ${args}`)
-		// 	} if (user[0]) {
-		// 		await interaction.reply('User already exists.')
-		// 	}
-		// }
-
-
-
+		if (!args) {
+			// Check if user already exists
+			const user = await User.findAll({
+				where: {
+					username: interaction.user.username
+				}
+			})
+			if (!user[0]) {
+				// Create new user
+				const newUser = await User.create({
+					username: interaction.user.username
+				})
+				await interaction.reply(`Created a new user with username: ${interaction.user.username}`)
+			} if (user[0]) {
+				// Alert sender the user already exists
+				await interaction.reply('User already exists.')
+			}
+		} else {
+			// Check if user already exists
+			const user = await User.findAll({
+				where: {
+					username: args
+				}
+			})
+			if (!user[0]) {
+				const newUser = await User.create({
+					username: args
+				})
+				await interaction.reply(`Created a new user with username: ${args}`)
+			} if (user[0]) {
+				await interaction.reply('User already exists.')
+			}
+		}
 	},
 };
