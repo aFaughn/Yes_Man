73e88af54ef7f0e7db74c45bb7a96ffea3fb79ba
aFaughn <allenfaughn@gmail.com>
2023-10-28 21:42:49 -0500

    removed placeholder messages for barely counter

--- commands/counters/barely.js
+++ commands/counters/barely.js
@@ -5,7 +5,6 @@ module.exports = {
 		.setName('carter_barely')
 		.setDescription('Increment\'s the amount of times Carter has made a "barely even know her" joke.'),
 	async execute(interaction) {
-		// await interaction.reply(`Carter has yet to help me implement PostgreSQL into this bot! For shame!`);
-		await interaction.reply(`Counters are temporarily disabled until I get linked to a **PostgreSQL** database. Sorry!`)	
+				
 	},
 };
