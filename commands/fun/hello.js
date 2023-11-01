b9ffa241688bbeadc61c7b9d3ba8f6ddf331ded5
aFaughn <allenfaughn@gmail.com>
2023-11-01 17:00:38 -0500

    Made hello.js a little more friendly and less robotic :)

--- commands/fun/hello.js
+++ commands/fun/hello.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder, InteractionCollector } = require("discord.js");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('hello')
-		.setDescription('replies with world'),
+		.setDescription('Say hello!'),
 	async execute(interaction) {
-		await interaction.reply('World!');
+		await interaction.reply(`<@${interaction.user.id}> Hi there! <:ZScheme:1111853072315060295>`);
 	},
 };
