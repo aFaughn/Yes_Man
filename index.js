25c9317649742fdddfc1257421a3a7b9d328ce37
aFaughn <allenfaughn@gmail.com>
2023-11-01 16:38:44 -0500

    removed server, documentation, and user commands

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ const client = new Client({
     ] 
  });
 
+ // Create a collection to store commands.
 client.commands = new Collection();
 
-// Dynamically loads all valid commands in ./commands
+// Dynamically loads all valid commands in ./commands.
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -46,11 +47,11 @@ for (const folder of commandFolders) {
 Object.keys(models).forEach(ele => {
   models[ele].associate(models);
 })
+
 //BEWARE! SETTING FORCE_DB_RESET TO 'TRUE' WILL --WIPE YOUR DB-- UPON ANY CHANGE!
 async function dbSync() {
   await db.sync({force: process.env.FORCE_DB_RESET === 'true' ? true : false})
 }
-
 dbSync()
 
 //DB Authentication
@@ -62,8 +63,21 @@ async function authDB() {
     console.error('Unable to connect to D.B! Error:', e)
   }
 }
-
 authDB();
+
+//Use node:fs to grab all events from events folder
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 // client.login logs the bot in and sets it up for use. You'll enter your token here.
 client.login(token);
