84f7408a19cfe3c44c725ba485b49805ef117283
aFaughn <allenfaughn@gmail.com>
2023-11-08 13:14:40 -0600

    bugfixing

--- commands/fun/leaderboard.js
+++ commands/fun/leaderboard.js
@@ -10,10 +10,27 @@ module.exports = {
 	async execute(interaction) {
 		const top3 = await User.findAll({
 			attributes: ['points', 'username'], 
-			order: sequelize.literal('max(points) DESC'),
+			order: [['points', 'DESC']],
 			limit: 3
 		})
+        let gamers = []
 
-		interaction.reply(` #1: ${top3[0].username} \n #2 ${top3[1].username} \n #3 ${top3[2].username}`)
+        // Check if at least 1 user exists.
+        if (!top3[0]) {
+            await interaction.reply('No users exist!')
+        } else {
+
+            gamers.push(`${top3[0].username} - ${top3[0].points}`)
+            
+            if (!top3[1]) {
+                gamers.push(`${top3[1].username} - ${top3[1].points}`)
+            }
+            if (!top3[2]) {
+                gamers.push(`${top3[2].username} - ${top3[2].points}`)
+            }
+
+            await interaction.reply(`#1: ${gamers[0]} points \n#2: ${gamers[1] ? gamers[1] : 'N/a' } points \n#3: ${gamers[2] ? gamers[2] : 'N/a' } points`)   
+
+        }
 	},
 };
