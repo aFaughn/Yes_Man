05aea00ac0ffb1750631af2faca7a5935dc371c1
aFaughn <allenfaughn@gmail.com>
2023-11-25 22:40:53 -0600

    Bot now pushes message to plex channel with requested title

--- commands/info/plexRequest.js
+++ commands/info/plexRequest.js
@@ -9,7 +9,10 @@ module.exports = {
                 .setDescription('Title of the film. Duh.')
                 .setRequired(true)),
 	async execute(interaction) {
+        // Destination channel
+        const channelId = '1125153614441238621'
 
-
+        const channel = client.channels.cache.get(channelId)
+        channel.send(`${interaction.user.username} requested ${interaction.options.getString('Title')}`)
 	},
 };
