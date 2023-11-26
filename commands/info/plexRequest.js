a791cf9a5dd9b7e6fdeed9077740f41cd951442d
aFaughn <allenfaughn@gmail.com>
2023-11-25 23:04:30 -0600

    modified plex request to have a modal

--- commands/info/plexRequest.js
+++ commands/info/plexRequest.js
@@ -1,18 +1,34 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('plex_request')
 		.setDescription('Creates a request for the plex server.')
         .addStringOption(option =>
-            option.setName('Title')
+            option.setName('title')
                 .setDescription('Title of the film. Duh.')
                 .setRequired(true)),
 	async execute(interaction) {
         // Destination channel
         const channelId = '1125153614441238621'
 
+        const markComplete = new ButtonBuilder()
+            .setCustomId('markcomplete')
+            .setLabel('Mark Completed')
+            .setStyle(ButtonStyle.Success)
+
+        const markBad = new ButtonBuilder()
+            .setCustomId('markBad')
+            .setLabel('Delete')
+            .setStyle(ButtonStyle.Danger)
+
+        const row = new ActionRowBuilder()
+            .addComponents(markComplete, markBad)
+
         const channel = client.channels.cache.get(channelId)
-        channel.send(`${interaction.user.username} requested ${interaction.options.getString('Title')}`)
+        channel.send({
+            content: `${interaction.user.username} requested ${interaction.option.getString('title')}`,
+            components: [row],
+        })
 	},
 };
