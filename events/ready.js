f6635335c1df84977d9ba270bf6bb7c5d5a45ca9
aFaughn <allenfaughn@gmail.com>
2023-11-29 02:10:22 -0600

    fixed typos in blackjack

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,22 @@
 const { Events } = require('discord.js');
+const apiKey = process.env.NASA_API_KEY
+const { nasaAPODChannel } = require('../package.json')
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Bot Logged in as ${client.user.tag}`);
+
+		const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, {
+			method: "GET",
+		})
+		.then(response => response.data)
+
+		const channel = await client.channels.fetch(nasaAPODChannel)
+		channel.send()
+
+
+
 	},
 };
