3a4cda675d70d0502783c98921c78cdfb1bb4b07
aFaughn <allenfaughn@gmail.com>
2023-11-29 02:44:59 -0600

    got rate limited... oopsie. :)

--- events/ready.js
+++ events/ready.js
@@ -9,19 +9,16 @@ module.exports = {
 		console.log(`Bot Logged in as ${client.user.tag}`);
 		const channel = await client.channels.fetch(nasaAPODChannel)
 
-		for (let i = 0; i < 365; i++) {
-			setTimeout(async () => {
+			setInterval(async () => {
 				const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, {
 					method: "GET",
 				})
 				.then(response => response.json())
 				.then(data => {
 					console.log(data)
-					channel.send(`Nasa Astronomy Picture Of The Day \n**${data.title}** \n${data.url} \n${data.explanation}
-					`)
+					channel.send(`Nasa Astronomy Picture Of The Day \n**${data.title}** \n${data.url} \n${data.explanation}`)
 				})
 			}, 86400000)
-		}
 
 	},
 };
