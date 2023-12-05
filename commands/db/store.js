e6899ff3da98e7355375cd1f9f68058e5a72677e
aFaughn <allenfaughn@gmail.com>
2023-12-04 19:30:34 -0600

    created a listing string and store object

--- commands/db/store.js
+++ commands/db/store.js
@@ -5,21 +5,34 @@ const purchase = async (cost, user) => {
 }
 
 const store = {
-    1: 'placeholder',
-    2: 'placeholder2'
+    'item 1': 1,
+    'item 2': 0,
+    'item 3': 999999999
 }
+
+const storeString = `__Yes-Man Store -- Emporium of Junk! -- Purchase now!__
+- item 1 - 1 point
+- item 2 - 0 points
+- item 3 - [SALE] - 999,999,999 points
+
+**Please take care to spell an item to purchase exactly as it appears in it's listing!*
+`
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('store')
 		.setDescription("Display's purchaseable items using points")
         .addStringOption(option => 
-            option.setName('arguement')
+            option.setName('item')
                 .setDescription('Items No. / List')
                 .setRequired(false)),
 	async execute(interaction) {
         const { client } = interaction
-        const channel = await client.channels.fetch(interaction.channel)
-        await channel.send(JSON.stringify(store))
+
+        if (!interaction.options.getString('item')) {
+            await interaction.reply(storeString)
+        } else {
+            
+        }
 	},
 };
