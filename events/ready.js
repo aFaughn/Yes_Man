beeb4c9053c0b5e97ac13e37e8eda9e5d84fc068
aFaughn <allenfaughn@gmail.com>
2023-12-04 23:52:05 -0600

    added routine to add members to db

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,18 @@
 const { Events } = require('discord.js');
+const { User } = require('../database/models');
 const apiKey = process.env.NASA_API_KEY
-const { nasaAPODChannel } = require('../package.json').config
+const { nasaAPODChannel, guildId } = require('../package.json').config
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
-		console.log(`Bot Logged in as ${client.user.tag}`);
-		const channel = await client.channels.fetch(nasaAPODChannel)
 
+		// Acknowledge Bot Ready
+		console.log(`Bot Logged in as ${client.user.tag}`);
+
+		// NASA APOD
+		const channel = await client.channels.fetch(nasaAPODChannel)
 			setInterval(async () => {
 				const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, {
 					method: "GET",
@@ -20,5 +24,17 @@ module.exports = {
 				})
 			}, 86400000)
 
+		// Create user entries for everyone in the server if there are no entries.
+		const dbCheck = await User.findAll()
+		if (!dbCheck[0]) {	
+			const guild = await client.guilds.fetch(`${guildId}`)
+			const members = await guild.members.fetch()
+			.then(response => response.forEach(member => {
+				if (!member.user.bot) {
+					User.create({username: member.user.username})
+					console.log(`Created DB entry for user ${member.user.username}`)
+				}
+			}))
+		}
 	},
 };
