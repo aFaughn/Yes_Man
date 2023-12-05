acbf44ac1bc0495df391c07f46837a6b0ed62683
aFaughn <allenfaughn@gmail.com>
2023-12-05 14:38:41 -0600

    work towards populating store

--- commands/db/store.js
+++ commands/db/store.js
@@ -2,17 +2,36 @@ const { SlashCommandBuilder, InteractionCollector } = require("discord.js");
 const { User } = require('../../database/models')
 
 const store = {
-    'item 1': 1,
-    'item 2': 0,
-    'item 3': 999999999
+    'gambacap 1': 1,
+    'gambacap 2': 1,
+    'gambacap 3': 1,
+    'gambacap 4': 1,
+    'gambacap 5': 1,
+    'gambacap 6': 1,
+    'gambacap 7': 1,
+    'gambacap 8': 1,
+    'gambacap 9': 1,
+    'gambacap 10': 1,
+    'pointscap 1': 1,
+    'pointscap 2': 1,
+    'pointscap 3': 1,
+    'pointscap 4': 1,
+    'pointscap 5': 1,
+    'pointscap 6': 1,
+    'pointscap 7': 1,
+    'pointscap 8': 1,
+    'pointscap 9': 1,
+    'pointscap 10': 1,
+    'Renew One-Time': 1e+21
 }
 
 const storeString = `__Yes-Man Store -- Emporium of Junk! -- Purchase now!__
-- gambacap - Increase your gamba cap to the next tier - 
-- item 2 - 0 points
-- item 3 - [SALE] - 999,999,999 points
+- gambacap - Increase your gamba cap to the next tier
+- pointscap - Increase your points cap to the next tier 
+- Renew One-Time - DOES NOT STACK renew a used one-time. - 999,999,999,999,999,999,999p
 
 **Please take care to spell an item to purchase exactly as it appears in it's listing!*
+When buying tiered Items (Marked with *T*) Make sure to include the tier you are purchasing. (e.g. /store gambacap 10)
 `
 
 module.exports = {
@@ -26,8 +45,10 @@ module.exports = {
         async execute(interaction) {
             const { client } = interaction
 
+            // Init user
             let user = await User.findOne({ where: { username: interaction.user.username}})
 
+            // Error handling for no user
             if (!user) {
                 await interaction.reply('No user found, please run /create_new_user')
             } else {
@@ -36,13 +57,21 @@ module.exports = {
                 const item = interaction.options.getString('item')
                 
                 if (!item) {
+
                     await interaction.reply(storeString)
+
+                } else if (item === 'Renew One-Time') {
+
+                    await interaction.reply('Nice try.')
+
                 } else {
-                    if (item === 'item 1') {
+
+                    if (store[item] !== undefined) {
                         await user.update({points: user.points - store[item]})
                         .then(user.save())
                         .then(interaction.reply(`Successfully purchased ${item} for ${store[item]} point(s)`))
                     }
+
                 }
             }
 	},
