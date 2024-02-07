159b83cf898ef3f940018033f1d0272e8cf5af34
aFaughn <allenfaughn@gmail.com>
2024-02-07 12:25:58 -0600

    added the ability to roll multiple dice

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -7,16 +7,36 @@ module.exports = {
             option.setName('die_type')
             .setDescription('Specify how many sides the die rolled should have')
             .setRequired(false)
+        )
+        .addIntegerOption(option => 
+            option.setName('die_amount')
+            .setDescription('Specify the amount of dice to roll')
+            .setRequired(false)
         ),
 	async execute(interaction) {
         let sides = await interaction.options.getInteger('die_type')
+        let rolls = await interaction.options.getInteger('die_amount')
+        if (rolls === null) rolls = 1;
         try {
-            if (sides === null) {
-                const outcome = Math.floor(Math.random() * 20)
-                await interaction.reply(`${outcome}`)
-            } else {
-                const outcome = Math.floor(Math.random() * sides)
-                await interaction.reply(`${outcome}`)
+            if (rolls === 1) {
+                if (sides === null) {
+                    const outcome = Math.floor(Math.random() * 20)
+                    await interaction.reply(`${outcome}`)
+                } else {
+                    const outcome = Math.floor(Math.random() * sides)
+                    await interaction.reply(`${outcome}`)
+                }
+            } else if (rolls > 1) {
+                let results = []
+                for (let i = 0; i < rolls; i++) {
+                    let roll = Math.random()
+                    results.push(roll)
+                }
+                let response = ''
+                results.forEach(res => {
+                    response.push(`[ ${res} ] `)
+                })
+                await interaction.reply(response)
             }
         } catch (e) {
             interaction.reply(`Something went wrong. Error: ${e}`)
