25472829521c5542871db695ae04a896704a7f9b
aFaughn <allenfaughn@gmail.com>
2024-03-05 11:51:54 -0600

    reworked how card totals are counted

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,6 +2,37 @@ const { Events } = require('discord.js');
 const { User } = require("../database/models");
 const blackjack = require('../commands/fun/blackjack');
 let film;
+let calculateTotal = (hand) => {
+	return hand.reduce((accum, cur) => {
+
+		if (cur === 1) {
+
+			accum += 11
+
+		}
+
+		if (cur === 11 || cur === 12 || cur === 13) {
+
+			accum += 10
+
+		}
+
+		if (cur === 14) {
+
+			accum += 1
+
+		}
+
+	})
+}
+const resetGame = {
+	wager: 0,
+	gameState: 0,
+	hands: {
+		dealer: [],
+		user: [],
+	}
+}
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -30,61 +61,72 @@ module.exports = {
 
 			// BlackJack ------------
 			if (interaction.customId === 'blackjackUserHit') {
+				//Hit Logic
 				const newCard = Math.floor(Math.random() * 13)
 				let user = await User.findOne({where: {username: interaction.user.username}});
 				let blackjack = JSON.parse(user.blackjack)
 				blackjack.hands.user.push(newCard)
 				await user.update({blackjack: JSON.stringify(blackjack)})
-				usertotal = blackjack.hands.user.reduce((accum, cur) => accum += cur)
-				if (usertotal > 21) {
-					const game = {
-						wager: 0,
-						gameState: 0,
-						hands: {
-							dealer: [],
-							user: [],
+
+				if (calculateTotal(blackjack.hands.user) > 21 && blackjack.hands.user.includes(1)) {
+
+					while (calculateTotal(blackjack.hands.user) > 21 && blackjack.hands.user.includes(1)) {
+
+						for (let i = 0; i < blackjack.hands.user.length; i++) {
+
+							if (blackjack.hands.user[i] === 1 && calculateTotal(blackjack.hands.user) > 21) {
+
+								blackjack.hands.user[i] = 14
+
+							}
+
 						}
+
 					}
-						await user.update({blackjack: JSON.stringify(game)})
-						await interaction.message.edit({ content: `\`Sorry, you went over 21 and busted (${usertotal})! Your wager has been deducted and your game has been reset.\``, components: []})
-						await interaction.deferUpdate()
+
 				} else {
-					await interaction.message.edit(` \`\`\`Dealer is showing: ${blackjack.hands.dealer[1]} \nYou are holding: ${blackjack.hands.user.toString(',')} \nTotal: ${usertotal}\`\`\``)
+
+					await interaction.message.edit(` \`\`\`Dealer is showing: ${blackjack.hands.dealer[1]} \nYou are holding: ${blackjack.hands.user.toString(',')} \nTotal: ${calculateTotal(blackjack.hands.user)}\`\`\``)
 					await interaction.deferUpdate()
+
 				}
 			}
 
 			if (interaction.customId === 'blackjackUserStay') {
+				// Stay Logic
+
 				let user = await User.findOne({where: {username: interaction.user.username}});
 				let blackjack = JSON.parse(user.blackjack)
 				await user.update({blackjack: JSON.stringify(blackjack)})
-				usertotal = blackjack.hands.user.reduce((accum, cur) => accum += cur)
-				let dealertotal = blackjack.hands.dealer.reduce((accum, cur) => accum += cur)
-				while (dealertotal < usertotal && dealertotal < 17) {
+
+				while (calculateTotal(blackjack.hands.dealer) < calculateTotal() && calculateTotal(blackjack.hands.dealer) < 17) {
 					
 					let newCard = Math.floor(Math.random() * 13)
 					blackjack.hands.dealer.push(newCard)
 				}
-				if (dealertotal > 21) {
+				if (calculateTotal(blackjack.hands.dealer) > 21) {
 
-					await interaction.message.edit({content: `The House Has Gone Bust! Points awarded to you and game reset!`, components: []})
-					await interaction.deferUpdate()
+					await interaction.message.edit({content: `The House Has Gone Bust! (${calculateTotal(blackjack.hands.dealer)}). \n Points are awarded to you and game has reset!`, components: []})
 
-				} else if (dealertotal === usertotal) {
+				} else if (calculateTotal(blackjack.hands.dealer) === calculateTotal(blackjack.hands.user)) {
 
-					await interaction.message.edit({content: `You and the dealer break even. Your game has been reset.`, components: []})
-					await interaction.deferUpdate()
+					await interaction.message.edit({content: `You (${calculateTotal(blackjack.hands.user)}) and the dealer (${calculateTotal(blackjack.hands.dealer)}) break even. Your game has been reset.`, components: []})
 
-				} else if (dealertotal > usertotal) {
-					await interaction.message.edit({content: `The house drew higher than you without busting, you lose!`})
-					await interaction.deferUpdate()
+				} else if (calculateTotal(blackjack.hands.dealer) > calculateTotal(blackjack.hands.user)) {
+
+					await interaction.message.edit({content: `The house (${calculateTotal(blackjack.hands.dealer)}) drew higher than you (${calculateTotal(blackjack.hands.user)}) without busting, you lose!`, components: []})
+
 				}
+
+				await user.update({blackjack: JSON.stringify(resetGame)})
+				await interaction.deferUpdate()
+
 			}
 
 			// Plex Requests ---------
 			if (interaction.customId === 'markComplete') {
 
-				channelId = '1125153614441238621'
+				let channelId = '1125153614441238621'
 				const channel = await client.channels.fetch(channelId)
 
 				for (let i = 0; i < interaction.message.content.length - 1; i++) {
