d1140fa643f8d251bed6df15d5f934f1c96e5d69
aFaughn <allenfaughn@gmail.com>
2024-04-19 20:16:35 -0500

    added slash command support to guilds outside the development guild

--- events/deployCommands.js
+++ events/deployCommands.js
@@ -37,10 +37,18 @@ const deployCommands = (dir) => {
             console.log(`Started refreshing ${commands.length} application (/) commands.`);
     
             // The put method is used to fully refresh all commands in the guild with the current set
+            // This is a faster way of refreshing commands on a development server.
             const data = await rest.put(
                 Routes.applicationGuildCommands(clientId, guildId),
                 { body: commands },
             );
+
+            //Push commands to all guilds the bot is in.
+            // These are only updated once per hour-ish and used for non-dev guilds.
+            await rest.put(
+                Routes.applicationCommands(clientId),
+                { body: commands },
+            )
     
             console.log(`Successfully reloaded ${data.length} application (/) commands.`);
         } catch (error) {
