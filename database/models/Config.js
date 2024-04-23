93c90bee0ecc21fb0d1ae94f586cc030c300375b
aFaughn <allenfaughn@gmail.com>
2024-04-23 10:31:56 -0500

    added Config table

---
+++ database/models/Config.js
@@ -0,0 +1,36 @@
+const db = require('../index');
+const { Model, DataTypes } = require('sequelize');
+
+class Config extends Model {
+    static associate () {
+
+    }
+}
+
+Config.init(
+    {
+        guildId: {
+            type: DataTypes.INTEGER,
+            unique: true
+        },
+        plexChannel: {
+            type: DataTypes.INTEGER,
+            unique: false
+        },
+        plexOwner: {
+            type: DataTypes.INTEGER,
+            unique: false
+        },
+        botModerators: {
+            type: DataTypes.JSON,
+            unique: false
+        },
+    },
+    {
+        paranoid: true,
+        sequelize: db,
+        modelName: 'Config'
+    }
+)
+
+module.exports = Config;
