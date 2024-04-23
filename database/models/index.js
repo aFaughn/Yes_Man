46ae7d5fa46e65fb733ce9967b70b0b7da511548
aFaughn <allenfaughn@gmail.com>
2024-04-23 10:20:22 -0500

    updated index imports

--- database/models/index.js
+++ database/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Counter = require('./Counter')
+const Guild = require('./Guild')
 
-module.exports = {User, Counter};
+module.exports = {User, Counter, Guild};
