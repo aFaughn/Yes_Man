63de9c56895d753715a5faf7f177758faecad972
aFaughn <allenfaughn@gmail.com>
2024-05-22 15:49:58 -0500

    creating migrations

--- database/models/index.js
+++ database/models/index.js
@@ -1,6 +1,46 @@
-const User = require('./User');
-const Counter = require('./Counter')
-const Guild = require('./Guild')
-const Config = require('./Config')
+// const User = require('./User');
+// const Counter = require('./Counter')
+// const Guild = require('./Guild')
+// const Config = require('./Config')
 
-module.exports = {User, Counter, Guild, Config};
+// module.exports = {User, Counter, Guild, Config};
+
+// Personal note: refer to inscryptr models/index.js and make sure its importing config properly.
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/database.js')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
