5e78b2d5f85417e185dbf5e01f011744d861562e
aFaughn <allenfaughn@gmail.com>
2024-06-16 22:10:29 -0500

    made give-points promise based and made messages ephemeral

--- commands/fun/givePoints.js
+++ commands/fun/givePoints.js
@@ -7,17 +7,16 @@ module.exports = {
 		.setDescription('Gives you 100 points if you have 0'),
 	async execute(interaction) {
         // Grab user who ran the command
-		const user = await User.findAll({where: {username: interaction.user.username}})
-
-        //Err handling -- No user
-		if (!user[0]) {
-            await interaction.reply('⚠ No user found! Please run /create_user first!')
-		}
-        if (!(user[0].points <= 0)) {
-            await interaction.reply(`⚠ You may only run this command when you have 0 points!`)
-        }
-
-        await user[0].update({ points: 100 });
-        await interaction.reply(`Gave 100 points to ${interaction.user.username}`)
+		User.findAll({where: {username: interaction.user.username}})
+        .then((data) => {
+            if (!data[0]) {
+                interaction.reply({ content: '⚠ No user found! Please run /create_user first!', ephemeral: true})
+            } else if (!data[0].points <= 0) {
+                interaction.reply({ content: `⚠ You may only run this command when you have 0 points!`, ephemeral: true })
+            } else {
+                data[0].update({ points: 100 })
+                interaction.reply({ content: `Gave 100 points to ${interaction.user.username}`, ephemeral: true })
+            }
+        })
 	},
 };
