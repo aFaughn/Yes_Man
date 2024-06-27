d87af9317f30b5aaa70a6350cd106b8d39c62f94
aFaughn <allenfaughn@gmail.com>
2024-06-27 01:59:05 -0500

    rewrote gamba

--- commands/fun/gamba.js
+++ commands/fun/gamba.js
@@ -9,89 +9,81 @@ module.exports = {
     .setDescription('Gamble imaginary points!')
     .addIntegerOption(option => 
         option.setName('wager')
-        .setDescription('How many points you\'re willing to risk! (Setting wager to 0 will wager ALL YOUR POINTS!)')
+        .setDescription('How many points you\'re willing to risk! (Setting wager to 0 will wager all your points <= 1000!)')
         .setRequired(true)),
         async execute(interaction) {
-            // Grab user who ran the command
-            const user = await User.findAll({where: {username: interaction.user.username}})
-            
-            // Grab user's inventory
-            // const inventory = JSON.parse(user[0].inventory)
-
-            //Err handling -- No user
-            if (!user[0]) {
-                interaction.reply('⚠ No user found! Please run /create_user first!')
-            }
-            
-            // Grab wager from args
             let wager = interaction.options.getInteger('wager')
-            if (wager === 0) {
-                wager = await user[0].points
-            }
+            User.findOne({ where: {username: interaction.user.username}})
+            .then(user => {
+                let userpoints = Number(user.points)
 
-            //Define what our wager is exactly
-            // if (wager === 'max') {
-            //      if (inventory.upgrades.gambacap === 0) {
-            //         wager = 50
-            //      } else {
-            //         wager = (inventory.upgrades.gambacap * 50) * inventory.upgrades.gambacap
-            //      }
-            // }
-
-            //User is at their current point cap
-            // if (user[0].points >= inventory.upgrades.pointscap) {
-            //     await interaction.reply(`You are at your point cap. Buy an upgrade with /store`)
-            //     return
-            // }
-
-            //User tried to wager more than they have
-            if (user[0].points < wager) { await interaction.reply('⚠ Cannot wager more than you have!')}
-
-            //Wager is not a number
-            // else if (typeof wager !== 'number' && wager !== 'max') {await interaction.reply(`⚠ Wager is not an integer. Your wager: "${wager}"`)}
-
-            //User has no points
-            else if (user[0].points <= 0 || wager === 0) {
-                await interaction.reply("⚠ You don't have any points to gamba with! Brokie!")
-            } else {
-                // Roll outcome
-                // Note to dev: Switch case cannot be used here practically due to logic constraints
-                const outcome = Math.floor(Math.random() * 100)
-                let reward;
-                if (outcome === 99 || outcome === 100) {
-                    
-                    reward = wager * 10
-                    user[0].update({ points: user[0].points + reward}) 
-                    interaction.reply(`[💎💎💎] \n (${outcome}) JACKPOT!!! You got ${reward}`)
-                    
-                } else if (outcome === 69) {
-                    
-                    reward = wager * 69
-                    user[0].update({ points: user[0].points + reward })
-                    interaction.reply(`[6️⃣9️⃣6️⃣9️⃣6️⃣9️⃣6️⃣9️⃣] \n LOLOLOLOLOLOLOLOL YOU ROLLED ${outcome} You got ${reward} points!`)
-                    
-                } else if (outcome >= 60 && outcome < 99 && outcome !== 69) {
-                    
-                    reward = wager * 3
-                    user[0].update({ points: user[0].points + reward })
-                    interaction.reply(`[🍒🍒🍒] \n (${outcome}) Big Winner! You got ${reward} points!`)
-                    
-                } else if (outcome >= 50 && outcome < 59) {
-                    
-                    reward = wager * 1.5
-                    user[0].update({ points: user[0].points + reward })
-                    interaction.reply(`[🍋🍋🍋] \n (${outcome}) Winner! You got ${reward} points!`)
-                    
-                } else if (outcome < 50 && outcome >= 45) {
-                    
-                    await interaction.reply(`[🍋💲💲] (${outcome}) Stale! You break even. (+0 points)`)
-                    
-                } else {
-                    
-                await user[0].update({ points: user[0].points - wager})
-                interaction.reply(`[🍋🍒🥝] \n (${outcome}) Loss! You lost your wager! -${wager} points!`)
-                
+                if (userpoints === 0) {
+                    interaction.reply({content: 'You have no points to wager.', ephemeral: true})
+                } 
+                // Check wager for max value
+                else if (wager === 0 && userpoints < 2000000000) {
+                    wager = 1000
+                } 
+                // Check if user is at or above the cap
+                else if (userpoints >= 2000000000) {
+                    interaction.reply({content: 'Sorry, you are at the points cap! Spend your points on something first.', ephemeral: true})
                 }
-            }
+                // If the user tried to wager more than they have, just wager all that is left <= 1000
+                else if (userpoints < wager && wager <= 1000) {
+                    wager = userpoints
+                }
+                // Finally we can begin the process of rolling an outcome.
+                 else {
+                    const outcome = Math.floor(Math.random() * 100)
+                    let reward;
+                    // Jackpot
+                    if (outcome === 99 || outcome === 100) {
+
+                        reward = wager * 10
+                        data.update({ points: userpoints + reward  })
+                        interaction.reply(`[💎💎💎] \n (${outcome}) JACKPOT!!! You got ${reward}`)
+
+                    }
+                    // 69
+                    else if (outcome === 69) {
+                    
+                        reward = wager * 69
+                        data.update({ points: userpoints + reward })
+                        interaction.reply(`[6️⃣9️⃣6️⃣9️⃣6️⃣9️⃣6️⃣9️⃣] \n LOLOLOLOLOLOLOLOL YOU ROLLED ${outcome} You got ${reward} points!`)
+                    
+                    }
+                    // Good Reward
+                    else if (outcome >= 60 && outcome < 99 && outcome !== 69) {
+                            
+                        reward = wager * 3
+                        data.update({ points: userpoints + reward })
+                        interaction.reply(`[🍒🍒🍒] \n (${outcome}) Big Winner! You got ${reward} points!`)
+                            
+                    }
+                    // Middling Reward
+                    else if (outcome >= 50 && outcome < 59) {
+                    
+                        reward = wager * 1.5
+                        data.update({ points: userpoints + reward })
+                        interaction.reply(`[🍋🍋🍋] \n (${outcome}) Winner! You got ${reward} points!`)
+                                
+                    }
+                    // Break Even
+                    else if (outcome < 50 && outcome >= 45) {
+                    
+                        interaction.reply(`[🍋💲💲] (${outcome}) Stale! You break even. (+0 points)`)
+                                
+                    }
+                    // Loss
+                    else {
+                    
+                        data.update({ points: userpoints - wager})
+                        interaction.reply(`[🍋🍒🥝] \n (${outcome}) Loss! You lost your wager! -${wager} points!`)
+                            
+                    }
+
+                }
+
+            })
         },
 };
