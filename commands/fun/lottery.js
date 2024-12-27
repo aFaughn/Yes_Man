cc67b05b747d19fa12de628aa8162bbd4444acaf
cbullard0414 <carterlbullard2@gmail.com>
2024-12-27 17:43:58 -0600

    Added Pick and Winning numbers readouts, as well as Payout readouts.

--- commands/fun/lottery.js
+++ commands/fun/lottery.js
@@ -8,29 +8,17 @@ module.exports = {
     .setName('scratchoff')
     .setDescription('Buy a lottery ticet!')
     .addIntegerOption(option =>
-<<<<<<< HEAD
         option.setName('firstpick')
-=======
-        option.SetName('firstPick')
->>>>>>> 1892c563b2adf308d35915645f83e7a1f7b89724
         .setDescription('Enter your first number between 1 and 10')
         .setRequired(true)
     )
     .addIntegerOption(option =>
-<<<<<<< HEAD
         option.setName('secondpick')
-=======
-        option.SetName('secondPick')
->>>>>>> 1892c563b2adf308d35915645f83e7a1f7b89724
         .setDescription('Select your second number between 1 and 10')
         .setRequired(true)
     )
     .addIntegerOption(option =>
-<<<<<<< HEAD
         option.setName('thirdpick')
-=======
-        option.SetName('thirdPick')
->>>>>>> 1892c563b2adf308d35915645f83e7a1f7b89724
         .setDescription('Pick your final number between 1 and 10')
         .setRequired(true)
     ),
@@ -48,15 +36,9 @@ module.exports = {
         }
 
         //Grab lotto ticket picks from args
-<<<<<<< HEAD
         let firstNum = interaction.options.getInteger('firstpick')
         let secondNum = interaction.options.getInteger('secondpick')
         let thirdNum = interaction.options.getInteger('thirdpick')
-=======
-        let firstNum = interaction.options.getInteger('firstPick')
-        let secondNum = interaction.options.getInteger('secondPick')
-        let thirdNum = interaction.options.getInteger('thirdPick')
->>>>>>> 1892c563b2adf308d35915645f83e7a1f7b89724
 
         //Declares array for picks
         let picks = [firstNum, secondNum, thirdNum]
@@ -84,20 +66,20 @@ module.exports = {
 
         if (matches <= 1)
         {
-            await interaction.reply("Not enough matches, sorry!")
+            await interaction.reply(`Not enough matches, sorry! \nWinning Numbers: ${wins} \nYour Picks: ${picks} \nPoints Awarded: 0`)
         }
 
         if (matches == 2)
             {
                 Payout = (Payout * 0.1)
                 user[0].update({points: User[0].points + Payout})
-                await interaction.reply("2 Matches!")
+                await interaction.reply(`2 Matches! \nWinning Numbers: ${wins} \nYour Picks: ${picks} \nPoints Awarded: ${Payout}`)
             }
 
         if (matches == 3)
             {
                 user[0].update({points: user[0].points + Payout})
-                await interaction.reply("3 Matches! You win the jackpot!")
+                await interaction.reply(`3 Matches! You win the jackpot! \nWinning Numbers: ${wins} \nYour Picks: ${picks} \nPoints Awarded: ${Payout}`)
             }
 
        
