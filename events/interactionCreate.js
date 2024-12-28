e7b321ca87de6b2266a4d8fca19cd3ebc7838213
aFaughn <allenfaughn@gmail.com>
2024-12-28 15:17:16 -0600

    removed dependencies for blackjack

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,5 @@
 const { Events } = require('discord.js');
 const { User, Config } = require("../database/models");
-const blackjack = require('../commands/fun/blackjack');
 let film;
 let calculateTotal = (hand) => {
 	let accum = 0;
@@ -53,70 +52,6 @@ module.exports = {
 
 		// Button listeners--------------------
 		} else if (interaction.isButton()) {
-
-			// BlackJack ------------
-			if (interaction.customId === 'blackjackUserHit') {
-				//Hit Logic
-				const newCard = Math.floor(Math.random() * 13)
-				let user = await User.findOne({where: {username: interaction.user.username}});
-				let blackjack = JSON.parse(user.blackjack)
-				blackjack.hands.user.push(newCard)
-				await user.update({blackjack: JSON.stringify(blackjack)})
-
-				if (calculateTotal(blackjack.hands.user) > 21 && blackjack.hands.user.includes(1)) {
-
-					while (calculateTotal(blackjack.hands.user) > 21 && blackjack.hands.user.includes(1)) {
-
-						for (let i = 0; i < blackjack.hands.user.length; i++) {
-
-							if (blackjack.hands.user[i] === 1 && calculateTotal(blackjack.hands.user) > 21) {
-
-								blackjack.hands.user[i] = 14
-
-							}
-
-						}
-
-					}
-
-				} else {
-
-					await interaction.message.edit(` \`\`\`Dealer is showing: ${blackjack.hands.dealer[1]} \nYou are holding: ${blackjack.hands.user.toString(',')} \nTotal: ${calculateTotal(blackjack.hands.user)}\`\`\``)
-					await interaction.deferUpdate()
-
-				}
-			}
-
-			if (interaction.customId === 'blackjackUserStay') {
-				// Stay Logic
-
-				let user = await User.findOne({where: {username: interaction.user.username}});
-				let blackjack = JSON.parse(user.blackjack)
-				await user.update({blackjack: JSON.stringify(blackjack)})
-
-				while (calculateTotal(blackjack.hands.dealer) < calculateTotal(blackjack.hands.user) && calculateTotal(blackjack.hands.dealer) < 17) {
-					
-					let newCard = Math.floor(Math.random() * 13)
-					blackjack.hands.dealer.push(newCard)
-				}
-				if (calculateTotal(blackjack.hands.dealer) > 21) {
-
-					await interaction.message.edit({content: `The House Has Gone Bust! (${calculateTotal(blackjack.hands.dealer)}). \n Points are awarded to you and game has reset!`, components: []})
-
-				} else if (calculateTotal(blackjack.hands.dealer) === calculateTotal(blackjack.hands.user)) {
-
-					await interaction.message.edit({content: `You (${calculateTotal(blackjack.hands.user)}) and the dealer (${calculateTotal(blackjack.hands.dealer)}) break even. Your game has been reset.`, components: []})
-
-				} else if (calculateTotal(blackjack.hands.dealer) > calculateTotal(blackjack.hands.user)) {
-
-					await interaction.message.edit({content: `The house (${calculateTotal(blackjack.hands.dealer)}) drew higher than you (${calculateTotal(blackjack.hands.user)}) without busting, you lose!`, components: []})
-
-				}
-
-				await user.update({blackjack: JSON.stringify(resetGame)})
-				await interaction.deferUpdate()
-
-			}
 
 			// Plex Requests ---------
 			if (interaction.customId === 'markComplete') {
