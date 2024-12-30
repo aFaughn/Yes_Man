30edc82a054c6ef1556584ea5df8114028640869
Carter B <carterlbullard2@gmail.com>
2024-12-29 22:47:47 -0600

    actually fixed gamba this time...i hope

--- commands/fun/gamba.js
+++ commands/fun/gamba.js
@@ -17,24 +17,30 @@ module.exports = {
             User.findAll({ where: {username: interaction.user.username}})
             .then(user => {
                 let userpoints = Number(user[0].points)
+                let allowroll = true
 
                 if (userpoints <= 0) {
                     interaction.reply({content: 'You have no points to wager.', ephemeral: true})
+                    allowroll = false
                 } 
 
-                if (wager == 0) {
+                if (wager == 0 && userpoints > 0) {
                     interaction.reply({content: `A minimum of 1 point must be wagered. \nIf you have zero points, use the /pointsgamba command.`, ephemeral: true})
+                    allowroll = false
                 }
                 // Check if user is at or above the cap
                 else if (userpoints >= 2000000000) {
                     interaction.reply({content: 'Sorry, you are at the points cap! Spend your points on something first.', ephemeral: true})
+                    allowroll = false
                 }
                 // If the user tried to wager more than they have, tell the user they don't have enough points.
-                else if (userpoints < wager) {
+                else if (userpoints < wager && userpoints > 0) {
                     interaction.reply({content: `not enough points, friend. Max bet: ${userpoints}`, ephemeral: true})
+                    allowroll = false
                 }
                 // Finally we can begin the process of rolling an outcome.
                  else {
+                    if (allowroll == true){
                     const outcome = Math.floor(Math.random() * 100)
                     let reward;
                     // Jackpot
@@ -76,12 +82,13 @@ module.exports = {
                                 
                     }
                     // Loss           |  || \n ||  |_
-                    else {
+                    else if (outcome <45){
                     
                         user[0].update({ points: user[0].points - wager  })
                         interaction.reply(`[🍋🍒🥝] \n (${outcome}) Loss! You lost your wager! -${wager} points!`)
                             
                     }
+                }
 
                 }
 
