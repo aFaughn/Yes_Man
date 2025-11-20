0e3d5c1f66a2d7f823ccbe4a973e2380e2ae29aa
Carter B <carterlbullard2@gmail.com>
2025-11-19 22:04:54 -0600

    Added TwoSum function.

---
+++ commands/fun/twosum.js
@@ -0,0 +1,68 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import {db} from '../../database/models/index.js';
+const { User } = db;
+export default {
+  data: new SlashCommandBuilder()
+    .setName("twosum")
+    .setDescription("Check if any two integers add up to the target number")
+    .addIntegerOption((option) =>
+      option
+        .setName("integer1")
+        .setDescription("Enter your first integer")
+        .setRequired(true)
+        .setMinValue(1)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("integer2")
+        .setDescription("Enter your second integer")
+        .setRequired(true)
+        .setMinValue(1)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("integer3")
+        .setDescription("Enter your third integer")
+        .setRequired(true)
+        .setMinValue(1)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("targetnumber")
+        .setDescription("Enter your target number")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    let intOne = interaction.options.getInteger("integer1");
+    let intTwo = interaction.options.getInteger("integer2");
+    let intThree = interaction.options.getInteger("integer3");
+    let targetNum = interaction.options.getInteger("targetnumber");
+
+    let intArray = [intOne, intTwo, intThree];
+    let foundPair = false;
+    let pair = [];
+
+    //Two Sum Logic
+    for (let i = 0; i < intArray.length; i++) {
+      for (let j = i + 1; j < intArray.length; j++) {
+        if (intArray[i] + intArray[j] === targetNum) {
+          foundPair = true;
+          pair = [intArray[i], intArray[j]];
+          break;
+        }
+      }
+      if (foundPair) break;
+    }
+
+    if (foundPair) {
+      return interaction.reply(
+        {content: `Yes! ${pair[0]} and ${pair[1]} add up to ${targetNum}.`, flags: MessageFlags.Ephemeral
+    });
+    } else {
+      return interaction.reply(
+        {content: `No, there are no two integers that add up to ${targetNum}.`, flags: MessageFlags.Ephemeral
+    });
+    }
+}
+}
